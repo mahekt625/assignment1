e245af1344ca06afe63847221e9e52afe2da4562
mahekt625 <98381285+mahekt625@users.noreply.github.com>
2024-08-07 22:10:33 +0530

    Update Name.py

--- Day1/Name.py
+++ Day1/Name.py
@@ -1,6 +1,6 @@
-name = "Bhushan Jadhav"
+name = "Mahek"
 age = 19
-fav_hobby = "play Cricket"
+fav_hobby = "dance"
 print("My name is "+name+". \nI am "+str(age)+" years old. \nI like to "+fav_hobby)
 ##
 #\n is used for next line 
